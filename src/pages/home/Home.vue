a0f467525c56c66d3e9cbfc4dc77dfde019cee01
dingyujie <1500541874@qq.com>
2021-12-17 11:58:24 +0800

    上传提示

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -82,7 +82,7 @@ export default {
       } else {
         sessionStorage.clear();
         localStorage.clear();
-        window.location.href="http://127.0.0.1:8080/login";
+        window.location.href="http://10.10.22.106/video";
         this.$Notice.warning({ title: "请检查工号或密码是否正确" });
 
       }
@@ -113,7 +113,7 @@ export default {
     console.log(code);
     if (code&&this.username==="未登陆") {
       // alert(document.location.href.split("?")[0]);
-      this.handleOnClickLogin(code,"http://127.0.0.1:8080/login");
+      this.handleOnClickLogin(code,"http://10.10.22.106/video");
     }
   }
 }
